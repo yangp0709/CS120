4cebf59997ce95ef4e48e52da51ee31e3f5b2693
Paul <paulyang0709@gmail.com / paulyang@college.harvard.edu>
2022-09-18 01:30:52 -0400

    Final Changes

--- fall2022/psets/ps0/ps0.py
+++ fall2022/psets/ps0/ps0.py
@@ -30,7 +30,14 @@ class BTvertex:
 # ... tree rooted at vertex v to the size of that subtree
 # Runtime: O(n)
 def calculate_sizes(v):
-    # Your code goes here
+    # Checks whether or not there exists a vertex
+    if v is None:
+        return 0
+
+    # Calculates the sizes of the left and right children adding the one vertex
+    else:
+        v.size = 1 + calculate_sizes(v.left) + calculate_sizes(v.right)
+        return v.size
     pass
 
 #
@@ -43,5 +50,36 @@ def calculate_sizes(v):
 # ... in disjoint trees that all have at most n/2 vertices
 # Runtime: O(h)
 def find_vertex(r): 
-    # Your code goes here
+
+    #Check if vertex exists
+    if r is None:
+        return 0
+
+    a = r 
+    n = r.size/2
+
+    #Check if the left and right children exist
+    if a.left is None and a.right is not None:
+        return a.right
+    #Check if the left or right children is larger
+    elif a.right is None and a.left is not None:
+        return a.left
+    
+    elif a.right is None and a.left is None:
+        return a
+    
+    #Look at the edge cases
+    if a.right is None and a.left is None:
+        parent= r.size - a.size
+
+        if parent.size < n and a.size < n:
+            return a
+    
+    if a.left.size and a.right.size < n:
+        return a
+    #If there's children then recursively go through the children that is larger
+    elif a.left.size > a.right.size:
+        find_vertex(a.left)
+    elif a.left.size < a.right.size:
+        find_vertex(a.right)
     pass
--- fall2022/psets/ps1/ps1.py
+++ fall2022/psets/ps1/ps1.py
@@ -16,7 +16,7 @@ In general, for each value of n and each universe size 'U' you will want to
 To graph, you can use a library like matplotlib or simply put your data in a Google/Excel sheet.
 A great resource for all your (current and future) graphing needs is the Python Graph Gallery 
 """
-
+arr = [211, 122, 213, 433]
 
 def merge(arr1, arr2):
     sortedArr = []
@@ -75,4 +75,83 @@ def BC(n, b, k):
     if n > 0:
         raise ValueError()
     return digits
+
+def RadixSort(univsize, b, A):
+
+    n = len(A)
+    output = []
+    k = math.ceil((math.log(univsize)/ math.log(b)))
+
+    for i in range (n):
+        flip = BC(A[i][0],b,k)
+        A[i].append((A[i][1], flip, A[i][0]))
+        del (A[i][1])
+    
+    for j in range (k):
+        for i in range (n):
+            A[i][0] = A[i][1][1][j]  
+        A = countSort(b, A)
+
+    for i in range (n):
+        output.append((A[i][1][-1], A[i][1][0]))
+           
+    return output    
+
+# A = [[123, 2], [478, 4], [972, 6], [821, 5]]
+# print(RadixSort(999, 10, A))
+
+
+
+
+
+for x in range(1, 17):
+    n = 2**x
+    for y in range(1, 21):
+        U = 2**y
+
+        A = []
+        mstime =[]
+        mctime = []
+        mrtime = []
+
+        for i in range(n):
+            k = random.randint(0, (U-1))
+            v = random.randint(0, U)
+            A.append([k,v])
+
+        for i in range(10):
+            t0 = time.time()
+            mergeSort(A)
+            t1 = time.time()
+            mstime.append(t1-t0)
+
+        for i in range(10):
+            t2 = time.time()
+            countSort(U,A)
+            t3 = time.time()
+            mctime.append(t3-t2)
+
+        for i in range(10):
+            t4 = time.time()
+            RadixSort(U,n,A)
+            t5 = time.time()
+            mrtime.append(t5-t4)
+
+        avg_m = sum(mstime)/10
+        avg_c = sum(mctime)/10
+        avg_r = sum(mrtime)/10
+
+        fastest = min(avg_m, avg_c, avg_r)
+
+        if fastest == avg_m:
+            name = "M"
+        elif fastest == avg_c:
+            name = "C"
+        else: name = "R"
+    
+        print("n:", math.log2(n), "U:", math.log2(U), name)
+        
+
+    
+
 
